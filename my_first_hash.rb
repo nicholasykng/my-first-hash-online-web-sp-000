9205d1abe7ca7aceb51978095afba80fc8ee9646
Nicholas Ng <nicholasykng@gmail.com>
2019-10-20 16:56:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,14 +2,9 @@ def my_hash
   together = {"umbrella" => "academy", "stranger" => "things"}
 end
 
-
-
 def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
-
-
-
 
 def retrieval
   shipping_manifest = {
@@ -18,8 +13,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]  
 end
 
 def adding
