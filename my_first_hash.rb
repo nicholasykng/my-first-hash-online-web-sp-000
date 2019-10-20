25ddfb5ff0b3fd49117a5323ddc7c858300ad46d
Nicholas Ng <nicholasykng@gmail.com>
2019-10-20 16:58:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,8 +12,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
-shipping_manifest["oil paintings"]  
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -22,14 +21,8 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+return shipping_manifest
 end
